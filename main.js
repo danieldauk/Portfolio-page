9f653db02795fabda6886a5f9678074aaecf5b3e
DanielDauk <danieldauk@gmail.com>
2018-04-09 09:12:54 +0300

    mobile menu corr

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 
   //close menu on click outside of menu and burger icon
-  $("body").click(function(){
+  $(".grid-container").click(function(){
     $("nav").removeClass("open");
     $(".burgerNav input").prop("checked", false);
   });
