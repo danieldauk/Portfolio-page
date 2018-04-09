122219ea02dd092718a13f6c90d58dab37ddd2a2
DanielDauk <danieldauk@gmail.com>
2018-04-09 09:05:12 +0300

    mobile menu correction

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 
   //close menu on click outside of menu and burger icon
-  $("html").click(function(){
+  $("body").click(function(){
     $("nav").removeClass("open");
     $(".burgerNav input").prop("checked", false);
   });
